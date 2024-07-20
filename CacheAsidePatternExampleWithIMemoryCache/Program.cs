d1286e70e2039d9e3762e7871cd90ea6082914e9
rei <67372053+reiugit@users.noreply.github.com>
2024-07-20 14:59:58 +0200

    Changed isCached to wasCached.

--- CacheAsidePatternExampleWithIMemoryCache/Program.cs
+++ CacheAsidePatternExampleWithIMemoryCache/Program.cs
@@ -9,16 +9,16 @@ var app = builder.Build();
 
 app.MapGet("/test-cacheaside/{id:int}", (int id, [FromServices] IMemoryCache cache) =>
 {
-    bool isCached = true;
+    bool wasCached = true;
 
     var response = cache.GetOrCreate(id, entry =>
     {
-        isCached = false;
+        wasCached = false;
         entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5);
         return $"Response for Id {id}";
     });
 
-    return new { id, response, isCached };
+    return new { id, response, wasCached };
 });
 
 app.Run();
