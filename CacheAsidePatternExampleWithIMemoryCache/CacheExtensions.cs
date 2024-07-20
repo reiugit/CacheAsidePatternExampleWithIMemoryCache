cf7c2e1f867c250017a304ee819ea1a90ad63f2e
rei <67372053+reiugit@users.noreply.github.com>
2024-07-20 15:27:35 +0200

    Added IMemoryCache extension.

---
+++ CacheAsidePatternExampleWithIMemoryCache/CacheExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace CacheAsidePatternExampleWithIMemoryCache;
+
+public static class CacheExtensions
+{
+    public static (bool, string) GetOrCreate(this IMemoryCache cache, int id, Func<string> stringFactory)
+    {
+        bool wasCached = true;
+
+        var response = cache.GetOrCreate(id, entry =>
+        {
+            wasCached = false;
+
+            entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5);
+            
+            return stringFactory();
+        });
+
+        return (wasCached, response!);
+    }
+}
--- CacheAsidePatternExampleWithIMemoryCache/Program.cs
+++ CacheAsidePatternExampleWithIMemoryCache/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using CacheAsidePatternExampleWithIMemoryCache;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,14 +10,7 @@ var app = builder.Build();
 
 app.MapGet("/test-cacheaside/{id:int}", (int id, [FromServices] IMemoryCache cache) =>
 {
-    bool wasCached = true;
-
-    var response = cache.GetOrCreate(id, entry =>
-    {
-        wasCached = false;
-        entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5);
-        return $"Response for Id {id}";
-    });
+    var (wasCached, response) = cache.GetOrCreate(id, () => $"Response for Id {id}");
 
     return new { id, response, wasCached };
 });
